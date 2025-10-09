f164562042e45f07c73ddf5f5d50e59cf1192de3
Alperen Aktuğ <aktugalperen9@gmail.com>
2025-10-09 23:45:43 +0300

    describe image

--- src/main/java/com/starter/SpringAI/ImageGenController.java
+++ src/main/java/com/starter/SpringAI/ImageGenController.java
@@ -6,9 +6,9 @@ import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiImageModel;
 import org.springframework.ai.openai.OpenAiImageOptions;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ImageGenController {
@@ -35,6 +35,19 @@ public class ImageGenController {
         ImageResponse response   = openAiImageModel.call(prompt);
 
         return response.getResult().getOutput().getUrl();
+
+
+    }
+
+
+    @PostMapping("/image/describe")
+    public String descImage(@RequestParam String query, @RequestParam MultipartFile file) {
+
+        return chatClient.prompt()
+                .user(us -> us.text(query)
+                        .media(MimeTypeUtils.IMAGE_JPEG, file.getResource()))
+                .call()
+                .content();
 
 
     }
