28085ca3171b115cc33bfe5b413fd6b5dd84eeb5
Alperen Aktuğ <aktugalperen9@gmail.com>
2025-10-09 23:26:30 +0300

    image options

--- src/main/java/com/starter/SpringAI/ImageGenController.java
+++ src/main/java/com/starter/SpringAI/ImageGenController.java
@@ -1,8 +1,13 @@
 package com.starter.SpringAI;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +17,25 @@ public class ImageGenController {
 
     private OpenAiImageModel openAiImageModel;
 
-    public ImageGenController(OpenAiImageModel openAiImageModel , ChatClient.Builder builder){
+    public ImageGenController(OpenAiImageModel openAiImageModel , OpenAiChatModel chatModel){
         this.openAiImageModel = openAiImageModel;
-        this.chatClient = builder
-                .build();
+        this.chatClient = ChatClient.create(chatModel);
+    }
+
+    @GetMapping("image/{query}")
+    public String genImage(@PathVariable String query){
+
+        ImagePrompt prompt = new ImagePrompt(query , OpenAiImageOptions.builder()
+                .quality("hd")
+                .height(1024)
+                .width(1024)
+                .style("natural")
+                .build());
+
+        ImageResponse response   = openAiImageModel.call(prompt);
+
+        return response.getResult().getOutput().getUrl();
+
+
     }
 }
