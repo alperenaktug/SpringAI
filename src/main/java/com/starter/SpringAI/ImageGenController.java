fe9e687e48e3d391335473e8184f654f965072f3
Alperen Aktuğ <aktugalperen9@gmail.com>
2025-10-09 22:53:53 +0300

    OpenAI image model

---
+++ src/main/java/com/starter/SpringAI/ImageGenController.java
@@ -0,0 +1,20 @@
+package com.starter.SpringAI;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ImageGenController {
+
+    private ChatClient chatClient;
+
+    private OpenAiImageModel openAiImageModel;
+
+    public ImageGenController(OpenAiImageModel openAiImageModel , ChatClient.Builder builder){
+        this.openAiImageModel = openAiImageModel;
+        this.chatClient = builder
+                .build();
+    }
+}
